9b2f4ab3cd034382f514bc3e6fa0f9202f16d249
Flavio <flcable@gmail.com>
2017-04-29 09:42:24 -0300

    New Features atualizar Data e hora de forma assincrona

---
+++ Correios/Controllers/AjaxController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Correios.Controllers
+{
+    public class AjaxController : Controller
+    {
+        // GET: Ajax
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public string dataHoraAtual()
+        {
+            return DateTime.Now.ToString("dd/MM/yyyy hh:mm:ss");
+        }
+    }
+}
